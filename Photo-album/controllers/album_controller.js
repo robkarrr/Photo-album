1f40eb6f2e695c8c83ce9d07d6667396b082903e
Robin Karlsson <robinkarlsson2001@gmail.com>
2022-03-08 15:17:28 +0100

    Added album controller

---
+++ Photo-album/controllers/album_controller.js
@@ -0,0 +1,136 @@
+/**
+ * Example Controller
+ */
+
+ const debug = require('debug')('books:example_controller');
+ const { matchedData, validationResult } = require('express-validator');
+ const models = require('../models');
+ 
+ /**
+  * Get all resources
+  *
+  * GET /
+  */
+ const index = async (req, res) => {
+     const all_albums = await models.Album.fetchAll();
+ 
+     res.send({
+         status: 'success',
+         data: all_albums,
+     });
+ }
+ 
+ /**
+  * Get a specific resource
+  *
+  * GET /:exampleId
+  */
+ const show = async (req, res) => {
+     const album = await new models.Album({ id: req.params.albumId })
+         .fetch();
+ 
+     res.send({
+         status: 'success',
+         data: album,
+     });
+ }
+ 
+ /**
+  * Store a new resource
+  *
+  * POST /
+  */
+ const storeAlbum = async (req, res) => {
+     // check for any validation errors
+     const errors = validationResult(req);
+     if (!errors.isEmpty()) {
+         return res.status(422).send({ status: 'fail', data: errors.array() });
+     }
+ 
+     // get only the validated data from the request
+     const validData = matchedData(req);
+ 
+     try {
+         const album = await new models.Album(validData).save();
+         debug("Created new example successfully: %O", album);
+ 
+         res.send({
+             status: 'success',
+             data: album,
+         });
+ 
+     } catch (error) {
+         res.status(500).send({
+             status: 'error',
+             message: 'Exception thrown in database when creating a new album.',
+         });
+         throw error;
+     }
+ }
+ 
+ /**
+  * Update a specific resource
+  *
+  * PUT /:exampleId
+  */
+ const updateAlbum = async (req, res) => {
+     const albumId = req.params.albumId;
+ 
+     // make sure example exists
+     const album = await new models.Album({ id: albumId }).fetch({ require: false });
+     if (!example) {
+         debug("Album to update was not found. %o", { id: albumId });
+         res.status(404).send({
+             status: 'fail',
+             data: 'Album Not Found',
+         });
+         return;
+     }
+ 
+     // check for any validation errors
+     const errors = validationResult(req);
+     if (!errors.isEmpty()) {
+         return res.status(422).send({ status: 'fail', data: errors.array() });
+     }
+ 
+     // get only the validated data from the request
+     const validData = matchedData(req);
+ 
+     try {
+         const updatedAlbum = await album.save(validData);
+         debug("Updated album successfully: %O", updatedAlbum);
+ 
+         res.send({
+             status: 'success',
+             data: album,
+         });
+ 
+     } catch (error) {
+         res.status(500).send({
+             status: 'error',
+             message: 'Exception thrown in database when updating a new album.',
+         });
+         throw error;
+     }
+ }
+ 
+ /**
+  * Destroy a specific resource
+  *
+  * DELETE /:exampleId
+  */
+ const destroy = (req, res) => {
+     res.status(400).send({
+         status: 'fail',
+         message: 'You need to write the code for deleting this resource yourself.',
+     });
+ }
+ 
+ module.exports = {
+     index,
+     show,
+     storeAlbum,
+     updateAlbum,
+     storePhoto,
+ }
+ 
